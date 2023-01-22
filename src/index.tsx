466101b8a829cd880ccab65b9dba2ab59159a528
Artem <artem.semenov.101@gmail.com>
2023-01-22 10:56:36 +0200

    add prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+
+let a = 10;
+console.log(a);
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode></React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-
+root.render(<React.StrictMode></React.StrictMode>);
