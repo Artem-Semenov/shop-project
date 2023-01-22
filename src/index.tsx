71edea5258f957a90ab0ca46c9706688c951c7a1
Artem <artem.semenov.101@gmail.com>
2023-01-22 11:40:56 +0200

    added react fragment

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-let a = 10;
-console.log(a);
+function Title() {
+  return <h1>Hello Ts</h1>;
+}
 
-/* const title = React.createElement('h1', {id: 1}, 'Hello React.js');
-console.log(title);
- */
-const title = <h1>Hello React.js</h1>;
+function Ul() {
+  return (
+    <>
+      <ul>
+        <li>List item 1 </li>
+        <li>List item 2 </li>
+        <li>List item 3 </li>
+      </ul>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore in sunt
+        tempora ratione, nulla commodi rerum saepe illum ipsum adipisci ipsam,
+        optio cupiditate natus aut delectus voluptatum neque eius ad.
+      </p>
+    </>
+  );
+}
 
-const content = (
-  <div>
-    {title}
-    {a + a}
-    <ul className="list">
-      <li>list item 1 </li>
-      <li>list item 2 </li>
-      <li>list item 3 </li>
-    </ul>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis libero
-      at id, ex, nulla laboriosam corporis quod optio provident iusto
-      consectetur porro dignissimos nisi ipsa amet debitis doloremque. Commodi,
-      odit!
-    </p>
-  </div>
-);
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-root.render(<React.StrictMode>{content}</React.StrictMode>);
+const App = () => {
+  return (
+    <div>
+      <Title />
+      <Ul />
+    </div>
+  );
+};
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
