7f7517775cdb34d163e919f43eb48f287e2705a1
Artem <artem.semenov.101@gmail.com>
2023-01-29 01:07:36 +0200

    added css file and dynamic classes according to const data boolean

--- src/List.tsx
+++ src/List.tsx
@@ -3,12 +3,8 @@ import "./List.css";
 type Props = {};
 
 export const List = (props: Props) => {
-  const styleObj = {
-    color: "red",
-    backgroundColor: "aqua",
-    padding: "15px",
-    fontSize: "20px",
-  };
+  const data = true;
+
   return (
     <>
       <ul>
@@ -19,26 +15,17 @@ export const List = (props: Props) => {
         <li>List item 2 </li>
         <li>List item 3 </li>
       </ul>
-      <p style={styleObj}>
+      <p className = {`content ${data ? 'test' : ''}`}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore in sunt
         tempora ratione, nulla commodi rerum saepe illum ipsum adipisci ipsam,
         optio cupiditate natus aut delectus voluptatum neque eius ad.
       </p>
-      <p
-        style={{
-          color: "red",
-          backgroundColor: "aqua",
-          padding: "15px",
-          fontSize: "20px",
-        }}
-      >
+      <p className = {`content ${data ? 'test' : ''}`}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore in sunt
         tempora ratione, nulla commodi rerum saepe illum ipsum adipisci ipsam,
         optio cupiditate natus aut delectus voluptatum neque eius ad.
       </p>
-      <p
-        className="content"
-      >
+      <p className = {`content`}>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore in sunt
         tempora ratione, nulla commodi rerum saepe illum ipsum adipisci ipsam,
         optio cupiditate natus aut delectus voluptatum neque eius ad.
