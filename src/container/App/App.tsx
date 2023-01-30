a0993395c3e74e9ef082b2d094c5bf5632e8c0c7
Artem <artem.semenov.101@gmail.com>
2023-01-30 11:26:15 +0200

    Added StyledEngineProvider component from MUI to make my own css styles priority

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,6 +1,7 @@
 import Footer from "container/Footer/Footer";
 import Header from "container/Header/Header";
 import Main from "container/Main/Main";
+import { StyledEngineProvider } from '@mui/material/styles';
 
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -8,10 +9,12 @@ type Props = {};
 const App = (props: Props) => {
   return (
     <>
+     <StyledEngineProvider injectFirst>
       <CssBaseline />
       <Header />
       <Main />
       <Footer />
+      </StyledEngineProvider>
     </>
   );
 };
