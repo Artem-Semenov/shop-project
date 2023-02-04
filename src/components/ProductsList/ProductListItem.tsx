e73b244dea1d2c21ca3fb6c331dc65f258799f96
Artem <artem.semenov.101@gmail.com>
2023-02-04 11:51:49 +0200

    added props to productlist items

--- src/components/ProductsList/ProductListItem.tsx
+++ src/components/ProductsList/ProductListItem.tsx
@@ -1,17 +1,28 @@
 import { Button, Card, CardActions, CardContent } from "@mui/material";
-import './ProductListItem.scss'
-type Props = {};
+import "./ProductListItem.scss";
+type Props = {
+  name: string;
+  type: string;
+  capacity: number;
+  desc: string,
+  price: number;
+};
 const ProductListItem = (props: Props) => {
-  return <Card className="product">
-    <CardContent>
-  <div className="product-title">Iphone 14 Pro</div>
-  <div className="product-desc">Type: phone</div>
-  <div className="product-features">Capacity: 256gb</div>
-  <div className="product-price">Price: 1440$</div>
-    </CardContent>
-    <CardActions>
-      <Button variant="outlined">Add to cart</Button>
-    </CardActions>
-  </Card>
-}
-export default ProductListItem
+  return (
+    <Card className="product">
+      <CardContent>
+        <div className="product-title">{props.name}</div>
+        <div className="product-desc">{props.desc}</div>
+        <div className="product-features">Type: {props.type}</div>
+        <div className="product-features">Capacity: {props.capacity}gb</div>
+        <div className="product-price">Price: {props.price}$</div>
+      </CardContent>
+      <CardActions>
+        <div className="btns-wrap">
+          <Button variant="outlined">Add to cart</Button>
+        </div>
+      </CardActions>
+    </Card>
+  );
+};
+export default ProductListItem;
--- src/components/ProductsList/ProductsList.tsx
+++ src/components/ProductsList/ProductsList.tsx
@@ -2,21 +2,25 @@ import { Grid, Typography } from "@mui/material";
 import ProductListItem from "./ProductListItem";
 
 type Props = {};
+const iphoneProps = {name: 'Iphone 14 Pro', desc: 'prefect phone', price: 1440, capacity: 256, type: 'phone'}
+const SamsungProps = {name: 'Samsung 14 Pro', desc: 'another phone another phone', price: 599, capacity: 256, type: 'phone'}
+const NokiaProps = {name: 'Samsung 14 Pro', desc: 'also phone another phone another phone another phone', price: 200, capacity: 256, type: 'phone'}
+
 const ProcutsList = (props: Props) => {
   return (
     <>
       <Typography variant="h3" component="div" align="center">
         List of products
       </Typography>
-      <Grid container spacing={4}>
+      <Grid container spacing={4} >
         <Grid item xs={12} md={4}>
-          <ProductListItem />
+          <ProductListItem  {...iphoneProps}/>
         </Grid>
         <Grid item xs={12} md={4}>
-          <ProductListItem />
+          <ProductListItem {...SamsungProps}/>
         </Grid>
         <Grid item xs={12} md={4}>
-          <ProductListItem />
+          <ProductListItem {...NokiaProps}/>
         </Grid>
       </Grid>
     </>
