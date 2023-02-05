90e4a164a378ce160acad876fe073369a5656c04
Artem <artem.semenov.101@gmail.com>
2023-02-05 11:31:45 +0200

    added item as class component

--- src/components/ProductsList/ProductListItem.tsx
+++ src/components/ProductsList/ProductListItem.tsx
@@ -1,5 +1,7 @@
 import { Button, Card, CardActions, CardContent, TextField } from "@mui/material";
 import "./ProductListItem.scss";
+import { Component } from 'react'
+
 type Props = {
   name: string;
   type: string;
@@ -8,6 +10,39 @@ type Props = {
   price: number,
   image: string,
 };
+
+
+class ProductListItem extends Component <Props> {
+  
+  render () {
+    return (
+      <Card className="product">
+        <CardContent>
+          <div className="product-image">
+            <img src={this.props.image} alt=""/>
+          </div>
+          <div className="product-title">{this.props.name}</div>
+          <div className="product-desc">{this.props.desc}</div>
+          <div className="product-features">Type: {this.props.type}</div>
+          <div className="product-features">Capacity: {this.props.capacity}gb</div>
+          <div className="product-price">Price: {this.props.price}$</div>
+          <div className="product-quantity">
+            <Button variant="outlined">-</Button>
+            <TextField size="small" value='1'></TextField>
+            <Button variant="outlined">+</Button>
+          </div>
+        </CardContent>
+        <CardActions>
+          <div className="btns-wrap">
+            <Button variant="outlined">Add to cart</Button>
+          </div>
+        </CardActions>
+      </Card>
+    );
+  }
+}
+
+/* 
 const ProductListItem = ({name, type, capacity, desc, price, image} : Props) => {
   return (
     <Card className="product">
@@ -33,5 +68,5 @@ const ProductListItem = ({name, type, capacity, desc, price, image} : Props) => 
       </CardActions>
     </Card>
   );
-};
+}; */
 export default ProductListItem;
