81a36d7475f200029b12af1b9b3e2528dcc5a264
Artem <artem.semenov.101@gmail.com>
2023-02-18 12:16:06 +0200

    added toggling paragraph

--- src/components/ProductsList/ProductListItem.tsx
+++ src/components/ProductsList/ProductListItem.tsx
@@ -20,12 +20,14 @@ type Props = {
 type State = {
   count: number;
   color: string;
+  show: boolean;
 };
 
 class ProductListItem extends Component<Props, State> {
   state = {
     count: 1,
-    color: 'green',
+    color: "green",
+    show: true,
   };
 
   onIncrementClick = (num: number) => {
@@ -38,12 +40,26 @@ class ProductListItem extends Component<Props, State> {
       count: prevState.count - num,
     }));
   };
-  
+
   onChangeColorClick = () => {
     this.setState((prevState) => ({
-      color: prevState.color === 'green' ? 'red' : prevState.color === 'red' ?'purple' : prevState.color === 'purple' ?'aqua' : 'green',
-    }))
-  }
+      color:
+        prevState.color === "green"
+          ? "red"
+          : prevState.color === "red"
+          ? "purple"
+          : prevState.color === "purple"
+          ? "aqua"
+          : "green",
+    }));
+  };
+
+  onShoAccordionBtnClick = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
   render() {
     const { name, type, capacity, desc, price, image } = this.props;
     return (
@@ -56,8 +72,31 @@ class ProductListItem extends Component<Props, State> {
           <div className="product-desc">{desc}</div>
           <div className="product-features">Type: {type}</div>
           <div className="product-features">Capacity: {capacity}gb</div>
-          <div><span style={{color: this.state.color}}> Color: {this.state.color}</span></div>
-          <button onClick={() => this.onChangeColorClick()}>Change color</button>
+          <div>
+            <span style={{ color: this.state.color }}>
+              {" "}
+              Color: {this.state.color}
+            </span>
+          </div>
+          <button onClick={() => this.onChangeColorClick()}>
+            Change color
+          </button>
+          <button
+            onClick={() => this.onShoAccordionBtnClick()}
+            style={{ display: "block", marginTop: "10px" }}
+          >
+            Show full description
+          </button>
+          {
+            this.state.show ? <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam
+            praesentium ducimus in nesciunt omnis iste facere voluptas deserunt
+            ipsum. Blanditiis perspiciatis a ipsum nostrum officiis obcaecati
+            impedit accusamus, expedita praesentium.
+          </p> : null
+          }
+          
+
           <div className="product-price">Price: {price}$</div>
           <div className="product-quantity">
             <Button
