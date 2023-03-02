de889024212873c71c5a8f60720ebb7152cddd01
Artem <artem.semenov.101@gmail.com>
2023-03-02 20:26:11 +0200

    added total counting in cart header

--- src/components/CartHeader/CartHeader.tsx
+++ src/components/CartHeader/CartHeader.tsx
@@ -4,20 +4,32 @@ type Props = {
   productsInCart: {
     [id: number]: number;
   };
-  productsObject?: 
-  {[id: number] : Product}
+  productsObject?: { [id: number]: Product };
 };
 
-const CartHeader = ({ productsInCart, 
-  productsObject = getProductsObject(productsArray) }: Props) => {
+const CartHeader = ({
+  productsInCart,
+  productsObject = getProductsObject(productsArray),
+}: Props) => {
   return (
-    <div>
-      {Object.keys(productsInCart).map((el, i) => (
-        <div key={i}>
-          {productsObject[parseInt(el)].name} : {productsInCart[parseInt(el)]}
-        </div>
-      ))}
-    </div>
+    <>
+      <div>
+        {Object.keys(productsInCart).map((el, i) => (
+          <div key={i}>
+            {productsObject[parseInt(el)].name} : {productsInCart[parseInt(el)]}
+          </div>
+        ))}
+      </div>
+      <div>
+        Total: $
+        {Object.keys(productsInCart).reduce((acc, el) => {
+          return (
+            acc +
+            productsObject[parseInt(el)].price * productsInCart[parseInt(el)]
+          );
+        }, 0)}
+      </div>
+    </>
   );
 };
 export default CartHeader;
