adcb6d51ab7477c5a12b026f91c85eb368239789
Artem <artem.semenov.101@gmail.com>
2023-03-04 10:36:31 +0200

    finished CartProductsList component

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,18 +1,14 @@
 import { Typography } from "@mui/material";
 import CartProductsList from "components/CartProductsList/CartProductsList";
 import CartTotal from "components/CartTotal/CartTotal";
-import productsArray, { getProductsObject, Product } from "utils/productsArray";
-
 type Props = {
   productsInCart: {
     [id: number]: number;
   };
-  productsObject?: { [id: number]: Product };
 };
 
 const CartPage = ({
-  productsInCart,
-  productsObject = getProductsObject(productsArray),
+  productsInCart
 }: Props) => {
   return (
     <>
